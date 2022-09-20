24997e3a0d743734ef820f7902b784b3b3a08bd6
Martin Engelhaus <martinengelhaus@gmx.de>
2022-09-20 15:36:03 +0200

    added player movement

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _orientation;
 
     //the different movement speeds
-    public float _runSpeed = 10f;
+    public float _walkSpeed = 10f;
     //public float _dashSpeed = 5f;
     //public float _sprintSpeed = 15f;
 
@@ -32,11 +33,45 @@ public class PlayerMovement : MonoBehaviour
     {
         _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        if (_direction.magnitude > 0) //if magnitude is above 0, the player is moving
+        {
+
+
+            // use animators for the corresponding movement direction
+            _animator.SetFloat("SpeedX", _direction.x);
+            _animator.SetFloat("SpeedY", _direction.y);
+            _animator.SetBool("isMoving", true);
+            FlipPlayer();
+
+        }
+
+        else //if magnitude is below 0, the player is not moving
+        {
+
+            
+            _animator.SetBool("isMoving", false);
+
+        }
+
+    }
+
+    private void FlipPlayer()
+    {
+        // character rotates by 180 degrees if he walks to the left (= less than zero velocity on the x axis)
+        if (_direction.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 180f, 0f));
+        }
+        // character rotates back if he walks towards the right 
+        else if (_direction.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+        }
     }
 
     private void FixedUpdate()
     {
-        _rb2d.velocity = _direction.normalized * _runSpeed;
+        _rb2d.velocity = _direction.normalized * _walkSpeed;
     }
 
 

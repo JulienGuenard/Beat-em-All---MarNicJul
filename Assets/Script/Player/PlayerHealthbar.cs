e387010911ac36b133d2480606ce5ff80834f1f2
Martin Engelhaus <martinengelhaus@gmx.de>
2022-09-26 16:19:49 +0200

    Added PlayerHealthbar and DeathAnimation

---
+++ Assets/Script/Player/PlayerHealthbar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthbar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float health)
+    {
+
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
+    }
+
+}

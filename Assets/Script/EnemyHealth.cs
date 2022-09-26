042efc2d89dbc75dddacb552baff8fd0618f92e5
Archer01 <julienguenard01@gmail.com>
2022-09-26 11:26:32 +0200

    enemy life bar
    enemy life bar

--- Assets/Script/EnemyHealth.cs
+++ Assets/Script/EnemyHealth.cs
@@ -1,24 +1,27 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : Enemy_Heritage
 {
     public int maxHealth = 100;
     public int currentHealth;
+    public Animator lifeBarAnimator;
 
     new void Awake()
     {
         base.Awake();
-        currentHealth = maxHealth; 
+        currentHealth = maxHealth;
+        lifeBarAnimator = transform.Find("Graphic").transform.Find("EnemyLifeBar").GetComponent<Animator>();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        lifeBarAnimator.gameObject.SetActive(true);
+        float lifePercent = 1f - ((float)currentHealth / (float)maxHealth);
+        lifeBarAnimator.Play("LifeBar_Fill", 0, lifePercent);
         if (currentHealth <= 0)
         {
+            lifeBarAnimator.gameObject.SetActive(false);
             Die();
         }
 
